21d967bafe476b22ffca92ca90dcf09666c098c8
sleep128 <1225831948@qq.com>
2018-04-15 23:05:51 +0800

    discard server

---
+++ netty-server/src/main/java/com/discard/DiscardServer.java
@@ -0,0 +1,65 @@
+package com.discard;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * 启动DiscardServerHandler的服务
+ */
+public class DiscardServer {
+    private String host;
+    private int port;
+
+    public DiscardServer(String host,int port) {
+        this.host = host;
+        this.port = port;
+    }
+    public void run() throws Exception{
+        //用来接收进来的连接
+        EventLoopGroup bossGroup = new NioEventLoopGroup();//(1)
+        //用来处理已经被接收的连接
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            //ServerBootstrap是一个启动NIO服务的辅助启动类
+            ServerBootstrap bootstrap = new ServerBootstrap();//(2)
+            bootstrap.group(bossGroup,workerGroup)
+                    //指定NioServerSocketChannel类来举例说明一个新的channel如何接收连接
+                    .channel(NioServerSocketChannel.class)//(3)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {//(4)
+                        @Override
+                        public void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new DiscardServerHandler());
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)//(5)
+                    .childOption(ChannelOption.SO_KEEPALIVE,true);//(6)
+            //绑定端口，开始接收进来你的连接
+            ChannelFuture f = bootstrap.bind(host,port).sync();//(7)
+            //等待服务器关闭，这个例子不会发生，直接关闭
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        String host;
+        int port;
+        if(args.length==2){
+            host = args[0];
+            port = Integer.parseInt(args[1]);
+        }else{
+            host = "127.0.0.1";
+            port = 8080;
+        }
+        System.out.println(String.format("sever start,host:%s,port:%d",host,port));
+        new DiscardServer(host,port).run();
+    }
+}
---
+++ netty-server/src/main/java/com/discard/DiscardServerHandler.java
@@ -0,0 +1,32 @@
+package com.discard;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+/**
+ * 处理服务端的channel
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("收到连接");
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            while (in.isReadable()){
+                System.out.println("收到消息"+(char)in.readByte());
+                System.out.flush();
+            }
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        //出现异常，关闭连接
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
